afe50b85cad5d5ec4d1ce7b38d4b231d6b4be7bb
TanishqNagar101 <tanishqnagar.data01@gmail.com>
2025-03-28 21:10:03 +0530

    Time: 4 ms (41.58%) | Memory: 9.7 MB (74.13%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,17 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        vector<char> s1;
-        vector<char> s2;
-        for(char i:s){
-            s1.push_back(i);
-        }
-        for(char i:t){
-            s2.push_back(i);
-        }
-
-       stable_sort(s1.begin(),s1.end());
-       stable_sort(s2.begin(),s2.end());
-       return s1 == s2;
+       sort(s.begin(),s.end());
+       sort(t.begin(),t.end());
+       return s == t;
     }
 };
