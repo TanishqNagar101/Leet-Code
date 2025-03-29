bd88a772958742ca643650405058438ad6ab38cf
TanishqNagar101 <tanishqnagar.data01@gmail.com>
2025-03-29 19:47:26 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (4.91%) - LeetSync

---
+++ 27-remove-element/remove-element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int place = 0;
+        for(int i = 0; i<nums.size();i++){
+            if(nums[i] != val){
+                nums[place] = nums[i];
+                place++;
+            }
+        }
+        
+        return place;
+        }
+
+};
