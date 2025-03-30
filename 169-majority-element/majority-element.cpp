4fc23166e0c307f2ed49f2ea5c6d96172fb9ea2a
TanishqNagar101 <tanishqnagar.data01@gmail.com>
2025-03-30 19:43:59 +0530

    Time: 0 ms (100.00%) | Memory: 27.9 MB (97.86%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     int majorityElement(vector<int>& c) {
+        int val;
+        int count = 0;
         int l = c.size();
-        unordered_map<int,int> mpp;
-        for(int i = 0; i<l;i++){
-            mpp[c[i]]++;
-        }
-        for(auto i:mpp){
-            if(i.second> l/2){
-                return i.first;
+        for(int i = 0;i<l;i++){
+            if(count == 0){
+                val = c[i];
+                count++;
+            }
+            else if(val == c[i]){
+                count++;
+            }
+            else{
+                count--;
             }
         }
-        return -1;
+        return val;
 
 }};
