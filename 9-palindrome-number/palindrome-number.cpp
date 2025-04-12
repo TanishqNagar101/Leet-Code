055eb69f155f02ad662bf8b31af8b3e283c55ace
TanishqNagar101 <tanishqnagar.data01@gmail.com>
2025-04-12 12:27:45 +0530

    Time: 7 ms (16.66%) | Memory: 8.6 MB (43.16%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
-    bool isPalindrome(int x) {
-        long rev {0};
-        long y{x};
-        if(x<0) return false;
-        while(x){
-            int temp = x%10;
-            x/=10;
-            rev = rev*10+temp;
+    int reverse(int x)
+    {
+        long add=0;
+        while(x!=0)
+        {   
+            add=add*10 + x%10;
+            x=x/10;
         }
-        cout<<y<<" "<<rev;
-        if(y == rev) return true;
-        
+        if(add<INT_MIN || add> INT_MAX)return 0;
+        return int(add);
+    }
+
+    bool isPalindrome(int x) {
+        if(x<0) return false;
+        int a = x;
+        int b=reverse(x);
+
+        if(b==a)return true;
+
         return false;
     }
 };
