9fc352e61847fe42f149b93b3185e7dd7f7c3ca0
TanishqNagar101 <tanishqnagar.data01@gmail.com>
2025-05-19 13:13:41 +0530

    Time: 2 ms (5.10%) | Memory: 8 MB (54.35%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -20,7 +20,12 @@ public:
         return cache[k];
     }
     int climbStairs(int n) {
-        vector<int> v(46,-1);
-        return foo(n,v);
+        vector<int> v(46,0);
+        v[1] = 1;
+        v[2] = 2;
+        for(int i = 3;i<=n;i++){
+            v[i] = v[i-1]+v[i-2];
+        }
+        return v[n];
     }
 };
