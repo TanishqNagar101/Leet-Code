7684f43597fe59a904449b863d1902f7465ece60
TanishqNagar101 <tanishqnagar.data01@gmail.com>
2025-05-19 18:30:20 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (22.30%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,13 +1,5 @@
 class Solution {
 public:
-    // int foo(int n, vector<int>& nums){
-    //     if(n>= nums.size()){
-    //         return 0;
-    //     }
-    //     int sum1 = nums[n]+foo(n+2,nums);
-    //     int sum2 = foo(n+1,nums);
-    //     return max(sum1,sum2);
-    // }
     int rob(vector<int>& nums) {
         int n = nums.size();
         if(n <=1) return nums[0];
@@ -16,10 +8,6 @@ public:
         v[1] = max(nums[1],nums[0]);
         for(int i=2;i<n;i++){
             v[i] = max(v[i-1],v[i-2]+nums[i]);
-        }
-        for(int i:v){
-            if(i == -1) break;
-            cout<<i<<endl;
         }
         return v[n-1];
     }
