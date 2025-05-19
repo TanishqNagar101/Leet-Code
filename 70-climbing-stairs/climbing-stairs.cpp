b8eb26ffee70700c963ef4d4029c2a6a77678b84
TanishqNagar101 <tanishqnagar.data01@gmail.com>
2025-05-19 13:10:30 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (56.67%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int foo(int k,vector<int> &cache){
+        if(cache[k] != -1) return cache[k];
+        if(k <= 0){
+            cache[0] = 0;
+        }
+        else if(k == 1){
+            cache[k] = 1;
+        }
+        else if(k == 2){
+            cache[k] = 2;
+        }
+        else{
+            int p = foo(k-1,cache);
+            int q = foo(k-2,cache);
+            cache[k] = p+q;
+        }
+        
+        return cache[k];
+    }
+    int climbStairs(int n) {
+        vector<int> v(100,-1);
+        return foo(n,v);
+    }
+};
