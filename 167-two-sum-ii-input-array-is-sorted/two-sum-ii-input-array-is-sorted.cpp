31164f0a45ed9acccf8dcee8e9f899465bee1ccf
TanishqNagar101 <tanishqnagar.data01@gmail.com>
2025-07-07 12:14:21 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (31.68%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,33 +1,17 @@
 class Solution {
 public:
-    int binarySearch(vector<int>& nums,int low,int high,int target){
-        int ans{-1};
-        while(low<=high){
-            int mid = (low+high)/2;
-            if(nums[mid] == target){
-                ans=mid;
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int low{0},high=nums.size()-1,ans;
+        vector<int>v;
+        while(low<high){
+            if(nums[low]+nums[high] == target){
+                v.push_back(low+1);
+                v.push_back(high+1);
                 break;
             }
-            else if(nums[mid] < target) low++;
+            else if(nums[low]+nums[high] < target) low++;
             else high--;
         }
-        return ans;
-    }
-    vector<int> twoSum(vector<int>& nums, int target) {
-        int prev{nums[0]};
-        for(int i=0;i<nums.size();i++){
-            if(prev == nums[i] && i!=0) continue;
-            int temp = target-nums[i];
-            if(temp >=nums[i]){
-                int k = binarySearch(nums,i+1,nums.size()-1,temp);
-                if(k != -1) return {i+1,k+1};
-            }
-            else{
-                int k = binarySearch(nums,0,i-1,temp);
-                if(k != -1) return {k+1,i+1};
-            }
-            prev = nums[i];
-        }
-        return {-1,-1};
+        return v;
     }
 };
