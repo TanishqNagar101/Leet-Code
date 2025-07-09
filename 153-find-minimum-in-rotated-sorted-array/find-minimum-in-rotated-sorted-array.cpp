619e7fe9a7ce76c8b8ef6376d871582c96d1fbd3
TanishqNagar101 <tanishqnagar.data01@gmail.com>
2025-07-09 11:40:50 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (79.36%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -6,14 +6,11 @@ public:
         if(nums[low]<nums[high] || n==1) return nums[low];
         if(n == 2) return nums[0]<nums[1]?nums[0]:nums[1];
         int ans{INT_MAX};
-        while(low<=high){
+        while(low<high){
             int mid = low+((high-low)/2);
-            cout<<mid<<endl;
-            if(mid > 0 && nums[mid]<nums[mid-1]) return nums[mid];
-            else if(mid == 0 && nums[mid+1]<nums[mid]) return nums[mid+1];
-            else if(nums[mid] <nums[high]) high =mid-1;
-            else low = mid+1;
+            if(nums[mid] > nums[high]) low = mid+1;
+            else high = mid;
         }
-        return -1;
+        return nums[high];
     }
 };
