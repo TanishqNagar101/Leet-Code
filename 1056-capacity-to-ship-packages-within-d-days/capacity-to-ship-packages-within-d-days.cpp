4488b77073a90a8bfdc325fb31b6528504a7b866
TanishqNagar101 <tanishqnagar.data01@gmail.com>
2025-07-09 10:15:45 +0530

    Time: 18 ms (15.29%) | Memory: 35.1 MB (34.48%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+
+    int validWeight(vector<int>& nums,int s,int d){
+        int sum{0},ans{0};
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            if(sum==s){
+                sum=0;
+                ans++;
+            }
+            else if(sum>s){
+                sum=0;
+                ans++;
+                sum = nums[i];
+            }
+        }
+        if(sum>0)ans++;
+        return ans>d?-1:ans;
+        
+    }
+    int shipWithinDays(vector<int>& weights, int days) {
+        int total_sum{0},m{INT_MIN};
+        for(int i:weights){
+            total_sum+=i;
+            m=max(i,m);
+        }
+        int low{m},high{total_sum},ans{total_sum};
+        while(low<=high){
+            int mid = low+((high-low)/2);
+            int k = validWeight(weights,mid,days);
+            cout<<"Low->"<<low<<" high->"<<high<<" Weight ->"<<k<<endl;
+            if(k!=-1){
+                ans = min(ans,mid);
+                high = mid-1;
+            }
+            else low = mid+1;
+        }
+        return ans;
+
+    }
+};
