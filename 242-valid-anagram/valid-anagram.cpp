04b88fb1859aa430628777f4e1f59f50c06b07ba
TanishqNagar101 <tanishqnagar.data01@gmail.com>
2025-07-16 16:20:31 +0530

    Time: 4 ms (39.15%) | Memory: 10 MB (11.82%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,8 +1,16 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-       sort(s.begin(),s.end());
-       sort(t.begin(),t.end());
-       return s == t;
+        unordered_map<char,int> m;
+        for(char j:s){
+            m[j]++;
+        }
+        for(char j:t){
+            m[j]--;
+        }
+        for(auto i:m){
+            if(i.second != 0) return false;
+        }
+        return true;
     }
 };
