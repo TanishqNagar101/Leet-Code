812592c0395680843acb4ec1508e08e831a6e779
TanishqNagar101 <tanishqnagar.data01@gmail.com>
2025-07-16 15:36:02 +0530

    Time: 2 ms (38.29%) | Memory: 17.8 MB (64.09%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -4,7 +4,6 @@ public:
         int n = num.size()-1;
         for(int i=n;i>=0;i--){
             int temp = num[i]-'0';
-            cout<<temp<<endl;
             if(temp%2 != 0) return num.substr(0,i+1);
         }
         return "";
