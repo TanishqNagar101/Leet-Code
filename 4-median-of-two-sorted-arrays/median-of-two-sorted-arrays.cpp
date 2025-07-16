fe21f4f0dc08a3f064c0a299fd260c68ad9939ce
TanishqNagar101 <tanishqnagar.data01@gmail.com>
2025-07-16 11:13:29 +0530

    Time: 33 ms (5.27%) | Memory: 95.2 MB (59.63%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int p1=0,p2=0;
-        int n = nums1.size()+nums2.size();
-        int cnt=0,req = n/2,prev,current;
-        while(p1<nums1.size() || p2<nums2.size()){
-            cout<<"Required COunt->"<<cnt<<" ,Current->"<<current<<" ,prev->"<<prev<<endl;
-            cnt++;
-            if(p1 == nums1.size()){
-                prev=current;
-                current = nums2[p2];
-                p2++;
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        if(n2<n1) return findMedianSortedArrays(nums2,nums1);
+        int l1,l2,r1,r2;
+        int low = 0, high = n1;
+        int half = (n1+n2)/2;
+        int first,second;
+        while(low<=high){
+            int mid1 = low + ((high-low)/2);
+            int mid2 = half-mid1;
+            if(mid1-1>=0) l1 = nums1[mid1-1];
+            else l1 = INT_MIN;
+            if(mid2-1>=0) l2=nums2[mid2-1];
+            else l2 = INT_MIN;
+            if(mid1<n1 && mid1>=0)r1=nums1[mid1];
+            else r1=INT_MAX;
+            if(mid2<n2 && mid2>=0)r2=nums2[mid2];
+            else r2=INT_MAX;
+            cout<<"The value "<<r1<<" "<<r2<<" "<<l1<<" "<<l2<<endl;
+            if(r1>=l2 &&r2>=l1){
+                first = max(l1,l2);
+                second = min(r1,r2);
+                break;
             }
-            else if(p2 == nums2.size()){
-                prev=current;
-                current = nums1[p1];
-                p1++;
-            }
-            else if(nums1[p1]<=nums2[p2]){
-                prev=current;
-                current = nums1[p1];
-                p1++;
-            }
-            else{
-                prev=current;
-                current = nums2[p2];
-                p2++;
-            }
-            if(cnt>req) break;
+            else if(!(r2>l1)) high = mid1-1;
+            else low = mid1+1;
         }
-        if(n%2 == 0) return (static_cast<double>(current)+prev)/2;
-        return current;
+        cout<<first<<" "<<second<<endl;
+        if((n1+n2)%2==0) return static_cast<double>(first+second)/2;
+        else return second;
     }
 };
