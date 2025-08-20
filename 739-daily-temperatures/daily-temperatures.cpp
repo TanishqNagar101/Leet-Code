ee6592167b84b141777080b2666742b7ac66ed23
TanishqNagar101 <tanishqnagar.data01@gmail.com>
2025-08-20 18:54:10 +0530

    Time: 53 ms (5.09%) | Memory: 169.3 MB (5.64%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-    vector<int> dailyTemperatures(vector<int>& temprature) {
-        int n = temprature.size();
-        vector<int> ans(n,0);
-        stack<int> st;
-        for(int i=n-1;i>=0;i--){
-            if(st.empty()){
-                st.push(i);
-                continue;
+    vector<int> dailyTemperatures( vector<int>& temperatures) {
+        deque<int> deque;
+        
+        vector<int> res(temperatures.size(), 0);
+
+        for (int i = temperatures.size() - 1; i >= 0; --i) {
+            if (deque.empty()) {
+                deque.push_front(i);
+                res[i] = 0;
+            } else {
+                while (!deque.empty() && temperatures[i] >= temperatures[deque.front()]) {
+                    deque.pop_front();
+                }
+
+                if (deque.empty()) {
+                    res[i] = 0;
+                } else {
+                    res[i] = deque.front() - i;
+                }
+
+                deque.push_front(i);
             }
-            if(temprature[st.top()]>temprature[i]){
-                ans[i]=st.top()-i;
-                st.push(i);
-                continue;
-            }
-            while(!st.empty() && temprature[st.top()]<=temprature[i]){
-                st.pop();
-            }
-            if(st.empty()){
-                st.push(i);
-                continue;
-            }
-            ans[i]=st.top()-i;
-            st.push(i);
-            
         }
-        return ans;
+
+        return res;
     }
-};
+}; 
+
+
