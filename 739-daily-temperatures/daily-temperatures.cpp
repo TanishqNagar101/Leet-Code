98019d144aa06f095ac812b22beb885a5813fdfa
TanishqNagar101 <tanishqnagar.data01@gmail.com>
2025-08-20 18:55:22 +0530

    Time: 28 ms (33.90%) | Memory: 107.3 MB (39.48%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -1,29 +1,14 @@
 class Solution {
 public:
-    vector<int> dailyTemperatures(vector<int>& temprature) {
-        int n = temprature.size();
-        vector<int> ans(n,0);
-        stack<int> st;
-        for(int i=n-1;i>=0;i--){
-            if(st.empty()){
-                st.push(i);
-                continue;
+    vector<int> dailyTemperatures(vector<int>& T) {
+        stack<int> s;
+        vector<int> ans(size(T));
+        for(int cur = 0; cur < size(T); cur++) {
+            while(size(s) and T[cur] > T[s.top()]) {    // pop till current temp < stack's top's temp. This maintains monotonic stack
+                ans[s.top()] = cur - s.top();           // cur day will be next warmer day for each element that's popped
+                s.pop();
             }
-            if(temprature[st.top()]>temprature[i]){
-                ans[i]=st.top()-i;
-                st.push(i);
-                continue;
-            }
-            while(!st.empty() && temprature[st.top()]<=temprature[i]){
-                st.pop();
-            }
-            if(st.empty()){
-                st.push(i);
-                continue;
-            }
-            ans[i]=st.top()-i;
-            st.push(i);
-            
+            s.push(cur);                                // push onto stack to find next warmer day for cur later on
         }
         return ans;
     }
