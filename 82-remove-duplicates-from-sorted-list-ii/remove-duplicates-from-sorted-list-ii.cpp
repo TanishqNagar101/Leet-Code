34416b697970f5048deed5812997d35a8222db25
TanishqNagar101 <tanishqnagar.data01@gmail.com>
2025-10-05 00:51:11 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (42.18%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
@@ -1,53 +1,25 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    void print(ListNode* head){
-        while(head){
-            cout<<head->val<<"->";
-            head=head->next;
-        }
-    }
-    ListNode* del(ListNode* curr){
-        ListNode* start = curr;
-        while(curr && curr->next && curr->val == curr->next->val){
-            curr = curr->next;
-        }
-        ListNode* nxt = curr->next;
-        if(curr && curr->next)cout<<curr->next->val;
-        while(start != curr){
-            ListNode* d = start;
-            start = start->next;
-            delete d;
-        }
-        delete curr;
-        if(nxt && nxt->next && nxt->val == nxt->next->val) nxt = del(nxt);
-        return nxt;
-
-    }
     ListNode* deleteDuplicates(ListNode* head) {
-        ListNode* dum = new ListNode(-1,head);
-        ListNode* prev = dum;
-        ListNode* curr = head;
-        while(curr && curr->next){
-            if(curr->val == curr->next->val){
-                prev->next = NULL;
-                curr = del(curr);
-                if(curr == NULL) return dum->next;
-                prev->next = curr;
+        if (!head || !head->next) return head;
+
+        ListNode* dummy = new ListNode(-1);
+        dummy->next = head;
+        ListNode* prev = dummy;
+        ListNode* cur = head;
+
+        while (cur && cur->next) {
+            if (cur->val == cur->next->val) {
+                while (cur->next && cur->val == cur->next->val) {
+                    cur = cur->next;
+                }
+                prev->next = cur->next; // Skip all duplicates
+            } else {
+                prev = prev->next; // Move to next unique node
             }
-            prev = curr;
-            if(curr) curr = curr->next;
+            cur = cur->next;
         }
-        print(dum);
-        return dum->next;
+
+        return dummy->next;
     }
 };
