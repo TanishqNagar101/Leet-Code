6ae1cc483c0efb15c08d3630bf27d9cd16940d9b
TanishqNagar101 <tanishqnagar.data01@gmail.com>
2025-12-02 18:47:57 +0530

    Time: 3 ms (80.11%) | Memory: 19 MB (31.35%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.py
@@ -0,0 +1,14 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        curr=0
+        for i in range(len(nums)):
+            if(nums[i]!=0):
+                temp=nums[i]
+                if(nums[curr]==0):
+                    nums[i]=0
+                nums[curr]=temp
+                curr+=1
+            
